abe8ce36b91ea4bcef3e174f974bcb2aad5c2a2e
Yhan Christian <yhan.christian@live.com>
2016-07-04 22:42:29 -0300

    Update bluetooth-dois-sensores.ino

--- bluetooth-dois-sensores/bluetooth-dois-sensores.ino
+++ bluetooth-dois-sensores/bluetooth-dois-sensores.ino
@@ -10,28 +10,26 @@ SoftwareSerial bluetooth(2, 3); //Defino RX pino 2 e pino 3
 #define sensor_temperatura A0  //Sensor de temperatura liga ao pino A0
 #define sensor_luminosidade A1  //Sensor de temperatura liga ao pino A1
 
-void setup()
-{
+void setup(){
 	Serial.begin(9600);         //Inicio comunicação serial
 	bluetooth.begin(115200);   // Inicio comunicação com módulo bluetooh, baud-rate 115200 
 }
 
-void loop()
-{
+void loop(){
   // --- Variaveis para leitura de sensores ---
 	int leitura_luminosidade = 0;
 	float leitura_temperatura = 0;
 	leitura_luminosidade = analogRead(sensor_luminosidade);
 	leitura_temperatura = (float(analogRead(sensor_temperatura)) * 5 / (1023)) / 0.01;
   // --- Exibição via serial --- 
-  Serial.print("Luminosidade: ");
+  	Serial.print("Luminosidade: ");
 	Serial.println(leitura_luminosidade);
-  Serial.print("Temperatura: ");
+  	Serial.print("Temperatura: ");
 	Serial.println(leitura_temperatura);
   // Print de dados módulo bluetooth ---
-  bluetooth.print("Luminosidade: ");
-  bluetooth.println(leitura_luminosidade);
-  bluetooth.print("Temperatura: ");
-  bluetooth.println(leitura_temperatura);
+  	bluetooth.print("Luminosidade: ");
+  	bluetooth.println(leitura_luminosidade);
+  	bluetooth.print("Temperatura: ");
+  	bluetooth.println(leitura_temperatura);
 	delay(1000); //atraso de 1s para nova leitura
 }
